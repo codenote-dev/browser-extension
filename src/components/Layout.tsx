883edac1b678ad810771b71856935cdf8b2408e5
Serhii Yukhnevych <s.yukhnevych@gmail.com>
2024-01-12 19:04:34 +0100

    fix the issue with background not beeing the whole height

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Theme, ThemeContext } from '~context/ThemeContext';
 export const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <ThemeContext.Provider value={Theme.dark}>
-            <div className="h-screen bg-[#202124]">{children}</div>
+            <div className="h-full min-h-screen bg-[#202124]">{children}</div>
         </ThemeContext.Provider>
     );
 };
