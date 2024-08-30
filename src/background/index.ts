23deffd76217ce25ff884d8b72b5877018d851a3
Serhii Yukhnevych <s.yukhnevych@gmail.com>
2024-08-31 00:43:31 +0200

    updated decription and removed unnecesary permissions

--- src/background/index.ts
+++ src/background/index.ts
@@ -3,21 +3,10 @@ import {
     toggleSidePanel,
 } from '~data/services/SidePanelService';
 
-const ORIGIN = 'https://github.com';
-
 chrome.runtime.onMessage.addListener((message) => {
-    // The callback for runtime.onMessage must return falsy if we're not sending a response
     if (message.type === 'open_side_panel') {
-        // chrome.windows.getCurrent((currentWindow) => {
-        //     chrome.sidePanel.open({ windowId: currentWindow.id });
-        // });
-
         openSidePanel();
     }
 });
-
-// chrome.sidePanel
-//     .setPanelBehavior({ openPanelOnActionClick: true })
-//     .catch((error) => console.error(error));
 
 chrome.action.onClicked.addListener(() => toggleSidePanel());
